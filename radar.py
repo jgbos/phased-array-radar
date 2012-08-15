123fc60ef076b8e9d2aede78c02f4df94b843ce7
Justin <jgoodwin314@gmail.com>
2012-08-14 23:18:14 -0400

    Added radar properties file

---
+++ radar.py
@@ -0,0 +1,15 @@
+# (m) 2.5/2 inch phase center spacing
+dx = 2.5*(1/12)*0.3048    
+
+# (Hz) LFM start frequency
+fStart = 2400e6                
+
+# (Hz) LFM stop frequency
+fStop = 2480e6                 
+
+# (Hz) carrier frequency
+fc = (fStart + fStop)/2        
+
+# (Hz) transmit bandwidth
+BW = fStop - fStart            
+
