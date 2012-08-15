403c654c927f02e515ac4270dba4691eeed76e21
Justin <jgoodwin314@gmail.com>
2012-08-15 09:02:53 -0400

    added new radar parameters

--- radar.py
+++ radar.py
@@ -4,8 +4,14 @@ numElements=14
 # number of samples from ADC
 numSamples=320
 
+# FFT Size for ADC to IQ
+NFFT = 2048
+
+# FFT Size for Range-Angle
+NFFT_ANGLE = 128
+
 # (m) 2.5/2 inch phase center spacing
-dx = 2.5*(1/12)*0.3048    
+delta_x = 2.5*(1/12.)*0.3048    
 
 # (Hz) LFM start frequency
 fStart = 2400e6                
@@ -18,3 +24,6 @@ fc = (fStart + fStop)/2
 
 # (Hz) transmit bandwidth
 BW = fStop - fStart            
+
+# (m) range resolution
+delta_r = 3e8 / (2*BW)
